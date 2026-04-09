aac1f5153826b73e1462f03ad3787251d058c5e5
JunYoungPark <166141760+JuNYoUnGPaark@users.noreply.github.com>
2026-04-09 15:36:24 +0900

    Add files via upload

---
+++ AS_study/week02-2.java
@@ -0,0 +1,36 @@
+package com.example.week01;  // 클래스 파일 위치
+
+import androidx.appcompat.app.AppCompatActivity;  // MainActivity를 사용해서 화면을 만들기 위한 부모 클래스
+
+import android.os.Bundle;  // 앱 상태나 임시 정보를 저장하는 클래스
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {  // 클래스 생성
+    Button btnGreet;
+    TextView resultText;
+    EditText nameInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {  // 앱이 실행되면 가장 먼저 실행되는 함수
+        super.onCreate(savedInstanceState);  // 부모 클래스의 create 실행
+        setContentView(R.layout.activity_main);  // Java파일과 XML파일 연결
+
+        btnGreet = findViewById(R.id.button);
+        resultText = findViewById(R.id.textView);
+        nameInput = findViewById(R.id.editTextText);
+
+        btnGreet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameInput.getText().toString();
+                String message = "안녕하세요, " + name + "님 반갑습니다!";
+
+                resultText.setText(message);
+            }
+        });
+    }
+
+}
